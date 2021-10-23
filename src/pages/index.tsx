7c9ecd0348678833a8556cdfbf7a694274a1f1d3
RafaelGoulartB <rafagoulartb@gmail.com>
2021-10-23 09:49:11 -0300

    📦 feat: adding tiitle in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
   )
 }
 
