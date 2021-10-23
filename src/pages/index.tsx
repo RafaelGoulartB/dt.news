1fa9b88d166a75303073c488db9b5158ef3ee958
RafaelGoulartB <rafagoulartb@gmail.com>
2021-10-23 10:20:24 -0300

    📦 feat: adding home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/home.module.scss'
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+// import { SubscribeButton } from '../components/SubscribeButton';
+// import { stripe } from '../services/stripe';
 
-const Home: NextPage = () => {
+import styles from './home.module.scss';
+
+export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
-
-export default Home
